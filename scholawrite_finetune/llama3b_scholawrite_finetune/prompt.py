b701eb6a73cd9371bea1f4e5b1400e9d18740f5c
Linghe-Wang <linghe567@gmail.com>
2024-12-22 22:18:23 -0500

    Remove generation prompt for llama3

--- scholawrite_finetune/llama3b_scholawrite_finetune/prompt.py
+++ scholawrite_finetune/llama3b_scholawrite_finetune/prompt.py
@@ -1,41 +1,3 @@
-persona_definition = {
-  "Idea Generation": "formulate and record initial thoughts and concepts.",
-  "Idea Organization": "select the most useful materials and demarcate those generated ideas in a visually formatted way.",
-  "Section Planning": "initially create sections and sub-level structures.",
-  "Text Production": "translate your ideas into full languages, either from your language or borrowed sentences from an external source.",
-  "Object Insertion": "insert visual claims of your arguments (e.g., figures, tables, equations, footnotes, itemized lists, etc.).",
-  "Cross-reference": "link different sections, figures, tables, or other elements within a document through referencing commands.",
-  "Citation Integration": "incorporate bibliographic references into a document and systematically link these references using citation commands.",
-  "Macro Insertion": "incorporate predefined commands orc packages into a LaTeX document to alter its formatting.",
-  "Fluency": "fix grammatical or syntactic errors in the text or LaTeX commands.",
-  "Coherence": "logically link (1) any of the two or multiple sentences within the same paragraph; (2) any two subsequent paragraphs; or (3) objects to be consistent as a whole.",
-  "Structural": "improve the flow of information by modifying the location of texts and objects.",
-  "Clarity": "improve the semantic relationships between texts to be more straightforward and concise.",
-  "Linguistic Style": "modify texts with your writing preferences regarding styles and word choices, etc.",
-  "Scientific Accuracy": "update or correct scientific evidence (e.g., numbers, equations) for more accurate claims.",
-  "Visual Formatting": "modify the stylistic formatting of texts, objects, and citations."
-}
-
-
-def text_gen_prompt(before_text, writing_intention):
-
-    user_prompt = f"""You are a computer science researcher with extensive experience of scholarly writing. Here, you are writing a research paper in natural language processing using LaTeX languages.
-
-You currently want to {persona_definition[writing_intention]}
-
-Below is the paper you have written so far. Given the paper information below and a corresponding scholarly writing intention, please revise or add to the text to fulfill this writing intention.
-
-You may insert, delete, or revise at appropriate place in the given paper. 
-
-Please give a complete output. Do not generate text that are nonsense or not related to the given paper information.
-
-{before_text}"""
-
-    return [
-        {"role": "user", "content": user_prompt}
-    ]
-
-
 def class_prompt(before_text):
     usr_prompt= f"""Here are all the possible writing intention labels:
 
@@ -93,23 +55,4 @@ Identify the most likely next writing intention of a graduate researcher when ed
     return [
         {"role": "user", "content": usr_prompt},
         {"role": "assistant", "content": intention_y}
-    ]
-
-def text_gen_prompt_train(before_text, writing_intention, after_text):
-
-    user_prompt = f"""You are a computer science researcher with extensive experience of scholarly writing. Here, you are writing a research paper in natural language processing using LaTeX languages.
-
-You currently want to {persona_definition[writing_intention]}
-
-Below is the paper you have written so far. Given the paper information below and a corresponding scholarly writing intention, please revise or add to the text to fulfill this writing intention.
-
-You may insert, delete, or revise at appropriate place in the given paper. 
-
-Please give a complete output. Do not generate text that are nonsense or not related to the given paper information.
-
-{before_text}"""
-
-    return [
-        {"role": "user", "content": user_prompt},
-        {"role": "assistant", "content": after_text}
     ]
