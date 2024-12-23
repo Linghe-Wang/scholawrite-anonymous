ba0fbfd6cef114c0e9c8c31f0566dcf2f4da9fe5
Linghe-Wang <linghe567@gmail.com>
2024-12-23 02:59:15 -0500

    Update evaluation tool docker container scripts

--- eval_tool/run_eval_app.sh
+++ eval_tool/run_eval_app.sh
@@ -1,17 +1,15 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/bash
+
+pipenv install
 
 # Start the Flask application in a tmux session
-tmux new-session -d -s eval_app "python3 eval_tool.py"
+tmux new-session -d -s eval_app "pipenv run python3 eval_tool.py"
 
 # Wait for Flask to start
 sleep 5
 
 # Start ngrok in another tmux session
-tmux new-session -d -s eval_ngrok "ngrok authtoken $FLASK_AT && ngrok http --url=still-frog-guided.ngrok-free.app 19198"
+tmux new-session -d -s eval_ngrok "ngrok --config ./ngrok.yml http --url=still-frog-guided.ngrok-free.app 12345"
 
 echo "Flask app started in tmux session 'eval_app'."
 echo "ngrok started in tmux session 'eval_ngrok'."
-
-# Keep the container running
-tail -f /dev/null
